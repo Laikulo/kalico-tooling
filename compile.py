be49e863258ec2beb6d5ed40f7a20b76dfb41f19
Aaron Haun <aaron@haun.guru>
2025-01-16 00:51:09 -0500

    fix: workaround for strange import

--- compile.py
+++ compile.py
@@ -3,8 +3,13 @@
 # This is meant to be executed in a seperate process to avoid importing anything from klippy into the main buld
 
 import logging
+import sys
+import os
 
-import klippy.chelper.builder as builder
+# The following is a nasty hack to work around Kalico importing printer.* in __init__
+sys.path.insert(0,f"{os.path.dirname(__file__)}/klippy")
+from chelper import builder
+
 
 logging.basicConfig(level=logging.DEBUG)
 
